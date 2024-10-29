892fddfce1dd5de197481612b14a78e239527633
Radhwane Namaoui <radhwane.namaoui@epita.fr>
2024-10-29 10:45:35 +0100

    feat: Secretariat table

---
+++ Interop/back/src/model/Secretariat.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Date, JSON
+from sqlalchemy.orm import relationship
+from src.model.Base import Base
+from src.model.CabinetMedical import CabinetMedical
+
+class Secretariat(Base):
+    __tablename__ = 'secretariat'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)  # name of the secretariat
+    email = Column(String, nullable=True)  # email of the secretariat
+    telephone = Column(String, nullable=True)  # telephone of the secretariat
+    habilitations = Column(String, nullable=True)  # habilitations of the secretariat
+    
+    # Relationships
+    cabinet_medical = relationship("CabinetMedical", back_populates="secretariat")
