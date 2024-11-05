fd4e2d49a1447552da52bbbfd8758622da4247a6
Radhwane Namaoui <radhwane.namaoui@epita.fr>
2024-11-05 11:33:18 +0100

    feat: add route for dossier in main

--- Interop/back/src/main.py
+++ Interop/back/src/main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.app.Medecin_routes import router as Medecin_router
 from src.app.Patient_routes import router as Patient_router
 from src.app.auth_routes import router as auth_router
+from src.app.Dossier_routes import router as Dossier_router
 from src.test.create_db import create_db, drop_all_data
 from src.utils.auth import protected_route
 
@@ -34,3 +35,6 @@ app.include_router(auth_router, prefix="/auth")
 
 app.include_router(Medecin_router, prefix="/medecins",
                    dependencies=[Security(protected_route(["admin", "Receptionist"]))])
+
+app.include_router(Dossier_router, prefix="/dossiers",
+                   dependencies=[Security(protected_route(["admin", "Doctor"]))])
